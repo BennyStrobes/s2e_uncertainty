78a6b2e6ff4281607a3b0bbc275e5487b37ae5a2
Ben Strober <bstrober3@gmail.com>
2026-06-25 12:20:40 -0400

    Update LD inverse file naming and summary file generation in integrate_eqtl_borzoi_ld_data.py.

--- eqtl_borzoi_integration_preprocessing/integrate_eqtl_borzoi_ld_data.py
+++ eqtl_borzoi_integration_preprocessing/integrate_eqtl_borzoi_ld_data.py
@@ -488,7 +488,7 @@ def generate_borzoi_summary_file(ld_summary_file, chrom_num, borzoi_input_dir, b
 	t.close()
 	return
 
-def generate_summary_file_ready_for_inv_ld(borzoi_summary_file, inverse_ld_extension_summary_file, LD_output_dir, eqtl_ss_output_dir):
+def generate_summary_file_ready_for_inv_ld(borzoi_summary_file, inverse_ld_extension_summary_file, LD_output_dir, eqtl_ss_output_dir, filter_LD=True):
 	head_count = 0
 	f = open(borzoi_summary_file)
 	t = open(inverse_ld_extension_summary_file,'w')
@@ -529,18 +529,22 @@ def generate_summary_file_ready_for_inv_ld(borzoi_summary_file, inverse_ld_exten
 			print('skipped gene')
 			continue
 
-		small_ld = ld_mat[valid_indices, :][:, valid_indices]
+		if filter_LD:
+			small_ld = ld_mat[valid_indices, :][:, valid_indices]
+			inv_ld_file = LD_output_dir + gene_name + '_inv_ld_padded_1e-3.npy'
+			if small_ld.shape[0] != analysis_snps:
+				print('assumption error: mismatch between LD matrix and analysis SNPs')
+				pdb.set_trace()
+		else:
+			small_ld = np.copy(ld_mat)
+			analysis_snps = small_ld.shape[0]
+			inv_ld_file = LD_output_dir + gene_name + '_big_inv_ld_padded_1e-3.npy'
 		small_ld_inv = compute_diagonal_padded_inverse_ld(small_ld, diag_padding=1e-3)
 
-		if small_ld.shape[0] != analysis_snps:
-			print('assumption eororoor')
-			pdb.set_trace()
 
-
-		inv_ld_file = LD_output_dir + gene_name + '_inv_ld_padded_1e-3.npy'
 		np.save(inv_ld_file, small_ld_inv)
 
-		t.write(data[0] + '\t' + data[1] + '\t' + data[2] + '\t' + data[3] + '\t' + data[4] + '\t' + inv_ld_file + '\t' + data[5] + '\t' + data[6] + '\t' + data[7] + '\n')
+		t.write(data[0] + '\t' + data[1] + '\t' + data[2] + '\t' + data[3] + '\t' + data[4] + '\t' + inv_ld_file + '\t' + data[5] + '\t' + data[6] + '\t' + str(analysis_snps) + '\n')
 
 
 	f.close()
@@ -564,33 +568,34 @@ gtex_v10_pc_genes_gtf = sys.argv[8]
 pc_genes = extract_dictionary_list_of_protein_coding_genes(gtex_v10_pc_genes_gtf, chrom_num)
 
 # Extract ordered list of tissues
-ordered_gtex_tissues = extract_ordered_gtex_tissues(eqtl_sumstats_dir, chrom_num)
+#ordered_gtex_tissues = extract_ordered_gtex_tissues(eqtl_sumstats_dir, chrom_num)
 
 ##############
 gtex_tissue_names_file=borzoi_output_dir + 'ordered_gtex_tissues_chr' + str(chrom_num) + '.txt'
 # Print to output
+'''
 t = open(gtex_tissue_names_file, 'w')
 t.write('tissue_name\n')
 for tissue_name in ordered_gtex_tissues:
 	t.write(tissue_name + '\n')
 t.close()
-
+'''
 
 # Load in eqtl sumstats object
-eqtl_ss_obj = load_in_eqtl_sumstats(eqtl_sumstats_dir, chrom_num, ordered_gtex_tissues, pc_genes)
+#eqtl_ss_obj = load_in_eqtl_sumstats(eqtl_sumstats_dir, chrom_num, ordered_gtex_tissues, pc_genes)
 
 
 # Print eQTL sumstat object to output
 gene_eqtl_summary_file = eqtl_ss_output_dir + 'gene_summary_chr' + str(chrom_num) + '.txt'
-print_eqtl_summary(gene_eqtl_summary_file, eqtl_ss_obj, eqtl_ss_output_dir, chrom_num)
+#print_eqtl_summary(gene_eqtl_summary_file, eqtl_ss_obj, eqtl_ss_output_dir, chrom_num)
 
 # Generate LD summary file
 ld_summary_file = os.path.join(LD_output_dir, 'gene_ld_summary_chr' + str(chrom_num) + '.txt')
-generate_gene_ld_summary(gene_eqtl_summary_file, plink2_genotype_data_dir, LD_output_dir, chrom_num)
+#generate_gene_ld_summary(gene_eqtl_summary_file, plink2_genotype_data_dir, LD_output_dir, chrom_num)
 
 
 # Finally generate borzoi summary file
-generate_borzoi_summary_file(ld_summary_file, chrom_num, borzoi_results_dir, borzoi_output_dir)
+#generate_borzoi_summary_file(ld_summary_file, chrom_num, borzoi_results_dir, borzoi_output_dir)
 borzoi_summary_file = os.path.join(borzoi_output_dir, 'gene_borzoi_summary_chr' + str(chrom_num) + '.txt')
 
 
@@ -601,6 +606,9 @@ inverse_ld_extension_summary_file = os.path.join(borzoi_output_dir, 'gene_borzoi
 
 
 
+# Get inverse LD Ready
+inverse_ld_extension_summary_file = os.path.join(borzoi_output_dir, 'gene_borzoi_summary_big_inv_ld_ready_chr' + str(chrom_num) + '.txt')
+generate_summary_file_ready_for_inv_ld(borzoi_summary_file, inverse_ld_extension_summary_file, LD_output_dir, eqtl_ss_output_dir, filter_LD=False)
 
 
 
